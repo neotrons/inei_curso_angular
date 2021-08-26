408d715b52ac9f5ca0187c9bfafba9f293cdd95b
Jose Ramirez <jcramireztello@gmail.com>
2021-08-26 17:58:58 -0500

    feat: my first component
    - create a hello word component

---
+++ src/app/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  template: `<h1>Hola mundo</h1>`,
+  styles: [`h1 { font-family: Lato; color: red; }`]
+})
+export class HelloComponent {}
