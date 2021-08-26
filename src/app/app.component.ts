f3408e83caea0102fbdef69c74c6a560be13d12a
Jose Ramirez <jcramireztello@gmail.com>
2021-08-26 18:07:30 -0500

    feat: Practical examples of data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Curso Angular Inei';
+  imageUrl = 'https://via.placeholder.com/150';
+  isUnchanged = true;
+  classes = 'aClassDemo';
+  buttonTitle = 'Ocultar imagen';
+  displayNone = '';
+
+  changeTitle(): void {
+    console.log(`Evento click para cambiar titulo`);
+    this.title = 'nuevo titulo';
+  }
+
+  hideImage(): void {
+    if (this.displayNone) {
+      this.displayNone = '';
+      this.buttonTitle = 'Ocultar Imagen';
+    }else {
+      this.displayNone = 'none';
+      this.buttonTitle = 'Mostrar Imagen';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -12,7 +14,8 @@ import { HelloComponent } from './hello.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
