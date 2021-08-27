2bbc8fcaa1f30be59b9f9c2d2e342d8dce289c9d
Jose Ramirez <jcramireztello@gmail.com>
2021-08-26 19:47:33 -0500

    feat: create component in custom module

---
+++ src/app/example/example-artifact/example-artifact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example-artifact',
+  templateUrl: './example-artifact.component.html',
+  styleUrls: ['./example-artifact.component.scss']
+})
+export class ExampleArtifactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ExampleChapterComponent } from './example-chapter/example-chapter.component';
+import { ExampleArtifactComponent } from './example-artifact/example-artifact.component';
 
 @NgModule({
   declarations: [
     ExampleChapterComponent,
+    ExampleArtifactComponent,
   ],
   imports: [
     FormsModule,
