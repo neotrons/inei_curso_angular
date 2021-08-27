b2848dd264bc154317781edc8f0ed2fb3848a809
Jose Ramirez <jcramireztello@gmail.com>
2021-08-26 20:11:08 -0500

    feat: example component life cycle

--- src/app/example/example-artifact/example-artifact.component.ts
+++ src/app/example/example-artifact/example-artifact.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-example-artifact',
   templateUrl: './example-artifact.component.html',
   styleUrls: ['./example-artifact.component.scss']
 })
-export class ExampleArtifactComponent implements OnInit {
+export class ExampleArtifactComponent implements OnChanges, OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(`life cycle: 1 constructor`);
   }
 
+  ngOnChanges(change: SimpleChanges): void {
+    console.log(`life cycle: 2 (listen) ngOnChanges ${change}`);
+  }
+
+  ngOnInit(): void {
+    console.log(`life cycle: 3 ngOnInit`);
+  }
+
+  ngOnDestroy(): void {
+    console.log(`life cycle: 4 ngOnDestroy`);
+  }
 }
