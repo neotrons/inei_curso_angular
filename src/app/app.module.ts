1cc1195eb0829edff070c8a75d7cdf903156b295
Jose Ramirez <jcramireztello@gmail.com>
2021-08-26 19:18:46 -0500

    feat: create custom module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { ExampleModule } from './example/example.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { HelloComponent } from './hello.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
