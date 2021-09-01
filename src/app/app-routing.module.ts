699cb6aa4e24371e41bd2421c214eb6ae4625b37
Jose Ramirez <jcramireztello@gmail.com>
2021-08-31 19:09:46 -0500

    feat: add routing for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello.component';
+import { FirstComponent } from './first/first.component';
+import { ExampleChapterComponent } from './example/example-chapter/example-chapter.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'hello',
+    component: HelloComponent,
+  },
+  {
+    path: 'first',
+    component: FirstComponent,
+  },
+  {
+    path: 'example',
+    component: ExampleChapterComponent
+  },
+  {
+    path: '**',
+    component: HelloComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
