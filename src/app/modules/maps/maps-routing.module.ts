754f5d50d6025c2e687a43154a5f5b4fdc91ba57
Jose Ramirez <jcramireztello@gmail.com>
2021-08-31 20:07:00 -0500

    feat: add page component

--- src/app/modules/maps/maps-routing.module.ts
+++ src/app/modules/maps/maps-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/reports/reports.module.ts
+++ src/app/modules/reports/reports.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReportsRoutingModule } from './reports-routing.module';
+import { HomePage } from './pages/home/home.page';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePage
+  ],
   imports: [
     CommonModule,
     ReportsRoutingModule
